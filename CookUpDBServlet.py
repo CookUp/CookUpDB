a63315cbd77b5c714e5d6bc6bffec79697b1a363
Volker <why@youneed.this>
2014-03-29 13:00:58 +0100

    create order working

--- CookUpDBServlet.py
+++ CookUpDBServlet.py
@@ -1,20 +1,23 @@
+import sys
 import web
 import json
 import logging
 
 import MealMatcher
+import DBMockUp
 
 urls = (
     "/CookUpDB", "CookUpDBServlet"
 )
 
 format_string = '%(asctime)s %(funcName)s:%(lineno)s %(levelname)s - %(message)s'
-logging.basicConfig(filename='/tmp/CookUpDBServlet.log',level=logging.DEBUG, format=format_string)
+logging.basicConfig(filename='/tmp/CookUpDBServlet.log',level=logging.DEBUG, format=format_string)#,stream=logging.StreamHandler(sys.stdout))
 
 class CookUpDBServlet(object):
     def __init__(self):
         self.log = logging.getLogger()
         self.matcher = MealMatcher.MealMatcher()
+        self.db_mockup = DBMockUp.DBMockUp()
 
     def GET(self):
         web.header("Content-type", "application/json; charset=utf-8")
@@ -30,9 +33,10 @@ class CookUpDBServlet(object):
         found_meal = self.matcher.get_meal_offer(in_json['meal'], in_json['location'])
 
         web.OK()
-        return json.dumps(found_meal)
+        return "hmmm... smakelig"#json.dumps(found_meal)
 
     def POST(self):
+        self.log.debug('post')
         web.header("Content-type", "application/json; charset=utf-8")
         in_json=None
         try:
@@ -43,12 +47,24 @@ class CookUpDBServlet(object):
             self.log.error(message)
             return '{"log_msg":message}'
 
+        # check if it's create (no buyer yet) or edit (buyer available)
+        edit = True if in_json['buyer'] else False
+
+        if edit:
+            self.close_order(in_json)
+        else:
+            self.create_order(in_json)
+
         web.OK()
         return json.dumps(in_json)
 
+    def close_order(self, in_json):
+        self.log.debug('close order: %s' % in_json)
+        
 
-
-
+    def create_order(self, in_json):
+        self.log.debug('create order: %s' % in_json)
+        self.db_mockup.create_order(in_json)
 
 if __name__ == "__main__":
         app = web.application(urls, globals())
--- DBMockUp.py
+++ DBMockUp.py
@@ -3,7 +3,11 @@ import json
 class DBMockUp(object):
 
     def __init__(self):
-        self.data=json.load(open('/home/volker/Dropbox/CookUp/DB.json'))
+        self.db_filename='/home/volker/Dropbox/CookUp/DB.json'
+        self.data=json.load(open(self.db_filename))
+
+    def update_db_file(self):
+        json.dump(self.data,open(self.db_filename,'w'),indent=4)
 
     def get_meals(self):
         return self.data['Meals']
@@ -13,5 +17,12 @@ class DBMockUp(object):
 
     def get_locations(self):
         return self.data['Locations']
+
+    def create_order(self, order):
+        new_id = 1+max([o['id'] for o in self.get_orders()]) 
+        order['id']=new_id
+        self.data['Orders'].append(order)
+        self.update_db_file()
+        
 
 
